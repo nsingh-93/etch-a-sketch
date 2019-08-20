a0b93db83486a6bb7fbdf187b60195e70a42c633
nsingh-93 <neils@vt.edu>
2019-08-20 14:52:46 -0400

    Grid display shows up now and need to get color change

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,5 +1,31 @@
-var container = document.querySelector('.container');
+let container = document.querySelector('.container');
+let containerPixelWidth = containerPixelHeight = 0;
 
 document.addEventListener('DOMContentLoaded', function() {
     container = document.querySelector('.container');
+    containerPixelWidth = (window.innerWidth * .8).toFixed(1);
+    containerPixelHeight = (window.innerHeight * .9).toFixed(1);
+    addGrid(container, 16);
 });
+
+function addGrid(container, cells) {
+    let gridItems = Array.from(document.querySelectorAll('.grid-item'));
+
+    if (gridItems.length > 0)
+    {
+        alert();
+    }
+
+    for (let row = 1; row <= cells; row++) {
+        for (let col = 1; col <= cells; col++) {
+            let div = document.createElement('div');
+            div.style.gridColumnStart = col;
+            div.style.gridColumnEnd = col + 1;
+            div.style.gridRowStart = row;
+            div.style.gridRowEnd = row + 1;
+            div.style.border = 'solid 1px black';
+            div.classList.add('grid-item');
+            container.appendChild(div);
+        }
+    }
+}
